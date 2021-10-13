9b3479bba63ac80dbd73557780864663e7bf48c8
Nikolay Khomich <nikolay@classaction.com>
2021-10-13 16:16:30 +0300

    Our Results mobile fix

--- web/themes/custom/nl/js/home.js
+++ web/themes/custom/nl/js/home.js
@@ -1,27 +1,53 @@
 (function ($, Drupal) {
 
     $(document).ready(function ($) {
+        // Our Results Carousel.
         $('.litigation-results').slick({
             dots: true,
             infinite: false,
             speed: 300,
             slidesToShow: 3,
-            slidesToScroll: 3
+            slidesToScroll: 3,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 768,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+            ]
         });
-    });
 
-    // Our Results Read More.
-    $(".view-results .read-more").click(function(e) {
-        let short = $(".inner-text .short-text");
-        let long = $(".inner-text .long-text");
+        // Our Results Read More.
+        $( ".view-results .read-more" ).each(function() {
+            $(this).click(function () {
+                let short = $(".inner-text .short-text");
+                let long = $(".inner-text .long-text");
 
-        short.addClass("hide");
-        short.removeClass("show");
+                short.addClass("hide");
+                short.removeClass("show");
 
-        long.addClass("show");
-        long.removeClass("hide");
+                long.addClass("show");
+                long.removeClass("hide");
 
-        $(".read-more").hide();
+                $(this).closest(".read-more").hide();
+            });
+        });
     });
 
 })(jQuery, Drupal);
